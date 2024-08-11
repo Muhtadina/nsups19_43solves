740b0e817e8be789dff5c4b6fa0e88e709ffcd30
TASIN <116006207+Muhtadina@users.noreply.github.com>
2024-08-12 03:04:20 +0600

    Initial 1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// NSUPS19: Solution to A = Solve Me First
+
+#include <iostream>
+int main()
+{
+    int integer1, integer2;
+    std::cin >> integer1;
+    std::cin >> integer2;
+    std::cout << integer1 + integer2 << std::endl;
+    return 0;
+}
