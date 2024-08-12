5fa75819fc6aa331d5987b0ce7a82f52e471f486
TASIN <116006207+Muhtadina@users.noreply.github.com>
2024-08-13 02:42:48 +0600

    First Commit

---
+++ PsAU.java
@@ -0,0 +1,26 @@
+/*
+ * PS19 Solution AU
+ */
+package com.mycompany.psau;
+import java.util.*;
+public class PsAU {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int a, b;
+        if(n%2==0)
+        {
+        a = (n/2) -1;
+        b = (n/2) +1;
+        System.out.println(a + " " + b);
+        }
+        else if(n%2!=0)
+        {
+        a = n/2;
+        b = (n/2) + (n%2);
+        System.out.println(a + " " + b);
+        }
+        
+    }
+}
