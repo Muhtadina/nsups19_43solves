70aea9721a268dd79984c12ee19573a1a57e915a
TASIN <116006207+Muhtadina@users.noreply.github.com>
2025-01-16 23:09:39 +0600

    AP

--- main.cpp
+++ main.cpp
@@ -1,37 +1,27 @@
-//NSUPS19 Solution AA: Slogan Learning of Princess
+//NSUPS19 Solution AP: Fox and Number Game
 
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int N;
-    cin >> N;
-    cin.ignore();
-    string*slogans = new string[N*2];
-    for(int i=0; i<N*2; i++)
+    int T;
+    cin >> T;
+    int num[T];
+    int gcd[T-1];
+    for(int i=0; i<T; i++)
     {
-        getline(cin, slogans[i]);
+        cin >> num[i];
     }
-    int Q;
-    cin >> Q;
-    cin.ignore();
-    string*firstL = new string[Q];
-    for(int i=0; i<Q; i++)
+    for(int i=0; i<T-1; i++)
     {
-        getline(cin, firstL[i]);
-        transform(firstL[i].begin(), firstL[i].end(), firstL[i].begin(),
-              ::toupper);
-        for(int j=0; j<N*2; j++)
-        {
-            transform(slogans[j].begin(), slogans[j].end(), slogans[j].begin(),
-              ::toupper);
-            if(firstL[i] == slogans[j])
-            {
-                cout << slogans[j+1] << endl;
-                break;
-            }
-            j++;
-        }
+        gcd[i] = __gcd(num[i], num[i+1]);
     }
+    int common = gcd[0];
+    for(int i=0; i<T-1; i++)
+    {
+        if(gcd[i]!=common)
+            common = __gcd(common, gcd[i]);
+    }
+    cout << common*T;
     return 0;
 }
